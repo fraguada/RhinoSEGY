39f1c55890a59e7eb66e20938b5af1d721e19b59
Luis Fraguada <luis@mcneel.com>
2019-01-03 09:27:10 +0100

    Add project files.

---
+++ RhinoSGY/RhinoSGYCommand.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using Rhino;
+using Rhino.Commands;
+using Rhino.Input;
+using Rhino.Input.Custom;
+
+namespace RhinoSGY
+{
+    public class RhinoSGYCommand : Command
+    {
+        public RhinoSGYCommand()
+        {
+            // Rhino only creates one instance of each command class defined in a
+            // plug-in, so it is safe to store a refence in a static property.
+            Instance = this;
+        }
+
+        ///<summary>The only instance of this command.</summary>
+        public static RhinoSGYCommand Instance
+        {
+            get; private set;
+        }
+
+        ///<returns>The command name as it appears on the Rhino command line.</returns>
+        public override string EnglishName
+        {
+            get { return "RhinoSGYCommand"; }
+        }
+
+        protected override Result RunCommand(RhinoDoc doc, RunMode mode)
+        {
+            // Usually commands in import plug-ins are used to modify settings and behavior.
+            // The import work itself is performed by the RhinoSGYPlugIn class.
+
+            return Result.Success;
+        }
+    }
+}
